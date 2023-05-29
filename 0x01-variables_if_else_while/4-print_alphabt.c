7840d08ff1901b7bb29080bf80a77b36bb4cb4a7
AlgoSierra <nangiroevanstioko>
2023-05-29 07:54:56 -0700

    5. Numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -19,4 +19,15 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
+	/**
+	 * Variable i is declared as a character to store
+	 * each letter of the alphabet.
+	 * 'for' loop is used to iterate from the letter 'a' to the letter 'z',
+	 * inclusively.
+	 * The loop will go through each letter of the lowercase alphabet.
+	 * Inside the loop, an if statement is used to check if the current
+	 * letter is not 'q' and not 'e'.
+	 * If the condition is true, the following code block will be executed.
+	 * The putchar function is used to print the current i.
+	 */
 }
