9dbe44118820a6103f9b4702a1b460e29f901169
AlgoSierra <nangiroevanstioko>
2023-06-04 14:40:04 -0700

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			j++;
+	printf("%d\n", j);
+	return (0);
+}
