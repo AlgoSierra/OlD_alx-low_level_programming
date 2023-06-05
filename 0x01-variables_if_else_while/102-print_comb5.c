da93f4760c7e2f2d9eea3f68e355f7fa77f3c481
AlgoSierra <nangiroevanstioko>
2023-06-05 05:15:37 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,28 @@
 
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		j = i + 1;
+		while (j <= 99)
 		{
-			putchar('0' + (i / 10));
-			putchar('0' + (i % 10));
+			k = !(i == 98 && j == 99);
+			putchar(48 + (i / 10));
+			putchar(48 + (i % 10));
 			putchar(' ');
-			putchar('0' + (j / 10));
-			putchar('0' + (j % 10));
+			putchar(48 + (j / 10));
+			putchar(48 + (j % 10));
 
-			if (i != 99 || j != 99)
+			if (k)
 			{
 				putchar(',');
 				putchar(' ');
 			}
+			j++;
 		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
